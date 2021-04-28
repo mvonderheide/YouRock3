62c21ea149b700ad3a1733500772f2e6d34ee0c8
Matthew Vonderheide <mattvonderheide@yahoo.com>
2021-04-28 16:37:04 -0500

    Initial Commit

---
+++ YouRock3/YouRock3App.swift
@@ -0,0 +1,17 @@
+//
+//  YouRock3App.swift
+//  YouRock3
+//
+//  Created by admin on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct YouRock3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
