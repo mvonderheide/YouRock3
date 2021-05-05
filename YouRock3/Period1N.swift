0d2387361820c21ac695ad7bf154004a3d14ba0a
Matthew Vonderheide <mattvonderheide@yahoo.com>
2021-05-05 16:49:47 -0500

    5/5 commit

--- YouRock3/Period1N.swift
+++ YouRock3/Period1N.swift
@@ -9,23 +9,38 @@ import SwiftUI
 
 struct Period1N: View {
     
-   
+    
     @State var rosterPeriod1N = [Student]()
+    @State var selectedStudent = Student()
+    @State var selectedStudentName = " "
+    @State var selectedIndex = 0
+    
     
     @State private var selectedFrameworkIndex = 0
     
-    var frameworks = ["Daniel Bang", "Smadar Bergman", "Mia Harris", "Kevin McQuown", "Miguel Sanchez", "Dimitri Villalobos"]
+    var names = ["Daniel Bang", "Smadar Bergman", "Mia Harris", "Kevin McQuown", "Miguel Sanchez", "Dimitri Villalobos"]
     
-//    struct Student: Hashable {
-//        var id = UUID()
-//        var firstName : String
-//        var lastName : String
-//        var numRocks : Int
-//    }
+    
+    //    struct Student: Hashable {
+    //        var id = UUID()
+    //        var firstName : String
+    //        var lastName : String
+    //        var numRocks : Int
+    //    }
     
     //var student1 : Student = Student(firstName: "Miguel", lastName: "Sanchez", numRocks: 4)
     
     var body: some View {
+        
+        var roster : [Student] = rosterPeriod1N
+        //names doesn't seem to populate in "onAppear" in time for the picker to use it so we are using  hard coded names
+        
+        // given a roster of Student, create an array of String of their first and last names - will that append to the back of the array or the front?
+        
+       // var names : [String] = [ ]
+        
+        
+        
         
         VStack {
             Spacer()
@@ -42,15 +57,24 @@ struct Period1N: View {
             //  })
             
             //Drop down menu starts here (includes frameworks and state variable)
-            Picker(selection: $selectedFrameworkIndex, label: Text("")) {
-                ForEach(0 ..< frameworks.count) {
-                    Text(self.frameworks[$0])
-                }
-            }
+            
+            
+            Picker(selection: $selectedIndex, label: Text("")) {
+                        ForEach(0 ..< names.count) {
+                           Text(self.names[$0])
+                        }
+                     }
+            
+            //onAppear doesn't seem to populate the names array by the time we get to this picker
+//            Picker(selection: $selectedIndex, label: Text("")) {
+//                ForEach(names, id: \.self) {
+//                    Text($0)
+//                }
+//            }
             NavigationLink(
                 destination: ChooseCategory(),
                 label: {
-                    Text("\(frameworks[selectedFrameworkIndex])")
+                    Text("\(names[selectedIndex])")
                 })
             
             
@@ -67,6 +91,14 @@ struct Period1N: View {
                     Text("Start Over")
                 })
         }
+        //This doesn't seem to populate the names roster in time for the picker to use it
+//        .onAppear{
+//            for s in roster {
+//                names.append(s.firstName +  " " + s.lastName)
+//
+//            }
+//            print(names)
+//        }
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .principal) {
